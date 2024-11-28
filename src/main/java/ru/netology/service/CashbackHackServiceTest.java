ea0d27d0a1b09ad974ac4eb759c64bda5b9f6491
ValentineShkarban <vals91@outlook.com>
2024-11-28 21:44:44 +0300

    Update CashbackHackServiceTest.java

--- src/main/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/main/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,1 +1,13 @@
+package ru.netology.service;
 
+import org.testng.Assert;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class CashbackHackServiceTest {
+  @Test
+  public void remainWhenLessThanBoundary() {
+  CashbackHackService service = new CashbackHackService();
+  Assert.assertEquals(10, service.remain(90))
+    }
+}

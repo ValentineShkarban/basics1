6775174872fac44cc560ed05c0ec59b1ab5e46aa
ValentineShkarban <vals91@outlook.com>
2024-12-09 20:17:53 +0300

    Update CashbackHackServiceTest.java

--- src/main/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/main/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,7 +9,7 @@ public class CashbackHackServiceTest {
   CashbackHackService service = new CashbackHackService();
     int amount = 1000;
     
-    int actual = service.calculateBonus(amount);
+    int actual = service.remainBonus(amount);
     int expected = 13; 
   Assert.assertEquals(expected, service.remain(amount:987);
     }

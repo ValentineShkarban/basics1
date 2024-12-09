cba2c0fd69cf04c4b5ad3a6e2c252a140b9d4d2d
ValentineShkarban <vals91@outlook.com>
2024-12-09 20:58:10 +0300

    Update CashbackHackServiceTest.java

--- src/main/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/main/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,16 +1,15 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.junit.Test;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
   @Test
   public void remainWhenLessThanBoundary() {
   CashbackHackService service = new CashbackHackService();
-    int amount = 1000;
-  
-    int actual = service.remain(amount: 987);
+
+    int actual = service.remain(987);
     int expected = 13; 
-  Assert.assertEquals(expected: 13, service.remain(amount:987));
+  Assert.assertEquals(expected, actual);
     }
 }

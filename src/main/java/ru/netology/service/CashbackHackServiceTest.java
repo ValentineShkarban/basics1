2188cc0ba87c39186c1a1e78900b39a243086ac3
ValentineShkarban <vals91@outlook.com>
2024-12-09 22:29:50 +0300

    Update CashbackHackServiceTest.java

--- src/main/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/main/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,8 @@
 package ru.netology.service;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     @Test
@@ -13,4 +14,25 @@ public class CashbackHackServiceTest {
 
         assertEquals(actual, expected);
     }
+
+    @Test
+    public void remainWhenMoreThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 700;
+        int actual = service.remain(1300);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remainWhenCashbackEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        assertEquals(actual, expected);
+    }
+
 }

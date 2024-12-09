1e649945cc6ef349f87026b1cc5409099b1d4f76
ValentineShkarban <vals91@outlook.com>
2024-12-09 19:53:15 +0300

    Update CashbackHackServiceTest.java

--- src/main/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/main/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,7 +7,7 @@ public class CashbackHackServiceTest {
   @Test
   public void remainWhenLessThanBoundary() {
   CashbackHackService service = new CashbackHackService();
-    int amount = 1000:
+    int amount = 1000;
 
    CashbackHackService.setamount(13);
     
